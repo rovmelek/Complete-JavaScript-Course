e6559a5586976f4afc9fbf3f51c66f5199314b5b
Kevin Tseng <hytseng@TRPG.org>
2020-03-31 14:35:11 -0700

    test quesion

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -1,2 +1,17 @@
-var userInput = prompt("Please input your answer: ")
-console.log('User input: ' + userInput)
+var userInput = prompt("Please input your answer: ");
+console.log('User input: ' + userInput);
+
+var questionBank = {
+    1: {
+        question: "Who is the teacher of this course?",
+        option: ["Jonas", "Mike", "Kevin"],
+        answer: "Jonas"
+    }
+};
+
+console.log(questionBank[1]["question"]);
+
+for (i = 0; i <= questionBank[1]["option"].length(); i++)
+{
+    console.log(questionBank[1]["option"][i]);
+};
