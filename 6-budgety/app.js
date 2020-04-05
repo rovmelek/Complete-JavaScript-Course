49733823d40c374714a0fe578806d7face28a85d
Kevin Tseng <hytseng@TRPG.org>
2020-04-04 20:31:14 -0700

    1st working beta version

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -1,7 +1,8 @@
 class budgetObj
 {
-    constructor(type, description, amount)
+    constructor(timestamp, type, description, amount)
     {
+        this.timestamp = timestamp;
         this.type = type;
         this.description = description;
         this.amount = amount;
@@ -17,13 +18,12 @@ class budgetControllerObj
 
     addItem(type, description, amount)
     {
-        console.log('Add new item');
-        this.items.push(new budgetObj(type, description, amount));
+        this.items.push(new budgetObj(Date.now(), type, description, amount));
     }
 
-    deleteItem()
+    deleteItem(timestamp)
     {
-        // do something
+        this.items = this.items.filter(item => item.timestamp !== parseInt(timestamp));
     }
 
     getItemByType(type)
@@ -55,11 +55,9 @@ class uiControllerObj
 
     getUserInput()
     {
-        console.log('Get user input');
         let type = $('.add__type').val();
         let description = $('.add__description').val();
         let amount = $('.add__value').val();
-        console.log(type, description, amount);
         return {type, description, amount};
     }
 
@@ -70,30 +68,31 @@ class uiControllerObj
 
     clearUserInput()
     {
-        console.log('Clear user input');
         $('.add__type').prop('selectedIndex', 0);
         $('.add__description').val('');
         $('.add__value').val('');
         this.setFocus('.add__description');
     }
 
-    updateItemList(type)
+    updateItemList(budgetController, type)
     {
         let itemList = budgetController.getItemByType(type);
-        console.log('Update income list');
-        console.log(itemList);
+
         if (itemList.length > 0)
         {
+            // clear the income/expense item list first
             (type === 'inc') ? $('.income__list').text('') : $('.expenses__list').text('');
+
+            // rebuild the income/expense item list
             for (let idx = 0; idx < itemList.length; idx++)
             {
-                console.log('Processing item number: ' + idx)
                 let typeString = (type === 'inc') ? 'income' : 'expense';
+                let id = itemList[idx].timestamp;
                 let desc = itemList[idx].description;
                 let amount = this.thousands_separators(itemList[idx].amount.toFixed(2));
                 let sign = (type === 'inc') ? '+' : '-';
                 let itemDivBlock =  `
-                    <div class="item clearfix" id="${typeString}-${idx}">
+                    <div class="item clearfix" id="${typeString}-${id}">
                         <div class="item__description">${desc}</div>
                         <div class="right clearfix">
                             <div class="item__value">${sign} ${amount}</div>
@@ -108,7 +107,7 @@ class uiControllerObj
         }
     }
 
-    updateTotal(type)
+    updateTotal(budgetController, type)
     {
         let total = budgetController.calculateTotal(type).toFixed(2);
         let target = (type === 'inc') ? '.budget__income--value' : '.budget__expenses--value';
@@ -116,7 +115,7 @@ class uiControllerObj
         $(target).text(sign + ' ' + this.thousands_separators(total));
     }
 
-    updateAvailableBudget()
+    updateAvailableBudget(budgetController)
     {
         let totalIncome = budgetController.calculateTotal('inc').toFixed(2);
         let totalExpense = budgetController.calculateTotal('exp').toFixed(2);
@@ -135,13 +134,10 @@ class uiControllerObj
         $('.budget__expenses--percentage').text('0%');
     }
 
-    displayItem()
+    updateMonth()
     {
-        console.log('Display items');
-        for (let i = 0; i < budgetController.items.length; i++)
-        {
-            console.log(budgetController.items[i]);
-        }
+        let month = Intl.DateTimeFormat('en-us', { month: 'long' }).format(new Date(Date.now()));
+        $('.budget__title--month').text(month);
     }
 }
 
@@ -149,37 +145,93 @@ class uiControllerObj
 function appController()
 {
     // only "mouse click" and "enter" will trigger number and UI update
-    if (event.type === 'click' || (event.type === 'keypress' && event.keyCode === 13))
+    if ((event.type === 'click'  && event.composedPath()[1].className === 'add__btn') || (event.type === 'keypress' && event.keyCode === 13))
     {
-        console.log(event);
         // get user input
         let {type, description, amount} = uiController.getUserInput();
-        // console.log(type, description, amount);
-
+        
         // update the budgets
         budgetController.addItem(type, description, parseFloat(amount));
 
         // call uiController() to update the total income or expense
-        uiController.updateTotal(type);
-
-        uiController.updateItemList(type);
+        uiController.updateTotal(budgetController, type);
+        uiController.updateItemList(budgetController, type);
 
         // clear the user inputs after updating the budgets
         uiController.clearUserInput();
 
         // update the available budget
-        uiController.updateAvailableBudget();
-        uiController.displayItem();
+        uiController.updateAvailableBudget(budgetController);
+        // uiController.displayItem(budgetController);
+    }
+    else if (event.type === 'click' && event.composedPath()[4].className === 'item clearfix')
+    {
+        let parts = event.composedPath()[4].id.split('-');
+        let type = (parts[0] === 'income') ? 'inc' : 'exp';
+        let timestamp = parts[1];
+        budgetController.deleteItem(timestamp);
+
+        // call uiController() to update the total income or expense
+        uiController.updateTotal(budgetController, type);
+        uiController.updateItemList(budgetController, type);
+        
+        // update the available budget
+        uiController.updateAvailableBudget(budgetController);
+    }
+    else 
+    {
+        console.log('Unknow event detected!');
+        console.log(event);
     }
 }
 
 // main()
-
 // initiate the environment
 let budgetController = new budgetControllerObj();
 let uiController = new uiControllerObj();
-uiController.resetNumber();
 
-// listen to the events and call the main controller function "appController()" to update number and UI
-$('.add__btn').on('click', appController);
-$('.add__value').on('keypress', appController);
+$(document).ready(function()
+{
+    uiController.updateMonth();
+    uiController.resetNumber();
+
+    // listen to events and call the main controller function "appController()" to update number and UI
+    $('.add__btn').on('click', appController);
+    $('.add__value').on('keypress', appController);
+    $('div.container.clearfix').on('click', '[id^=income]', appController);
+    $('div.container.clearfix').on('click', '[id^=expense]', appController);
+});
+
+/*
+function debugController()
+{
+    console.log('Debug: event info');
+    console.log(event);
+    // console.log(event.data);
+    // console.log(event.target);
+    // console.log(event.path);
+    // console.log(typeof event.composedPath()[4]);
+    console.dir(event.composedPath()[1]);
+    console.dir(event.composedPath()[4]);
+    console.log(event.composedPath()[4].id);
+    console.log(event.composedPath()[4].className);
+    console.log(event.composedPath()[4].children[0].innerText);
+    console.log(event.composedPath()[4].children[1].innerText);
+    // console.log('hash: ' + event.composedPath()[4].children[0].innerText.hashCode())
+    // console.log('timestamp: ' + Date.now());
+
+}
+*/
+// jQuery tests
+// $(document).on('click', '.add__btn', {extra: 'add'}, appController);
+// $(document).on('keypress', '.add__value', {extra: 'add'}, appController);
+// $('div.container.clearfix').on('click', '[id^=income]', debugController);
+// $('div.income__list').on('click', debugController);
+// $('div.container.clearfix').on('click', '[id^=income]', debugController);
+// $('div.container.clearfix').on('click', '[id^=expense]', debugController);
+// $('.add__btn').click({action: 'add'}, appController);
+// $('.add__value').keypress({action: 'add'}, appController);
+// $('div.container.clearfix').click('[id^=income]', debugController);
+// $('div.container.clearfix').click('[id^=expense]', debugController);
+// $('div.container.clearfix').on('click', '[id^=income]', {action: 'delete'}, debugController);
+// $('div.container.clearfix').on('click', '[id^=expense]', {action: 'delete'}, appController);
